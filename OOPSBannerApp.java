0b7f758bbcc57a874c69ccaa08a868a0adb3c700
BHANU GUPTA <guptabhanu3636@gmail.com>
2026-02-17 09:31:11 +0530

    UC2: Banner using print and concatenation

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,19 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        // UC1: Simple print statement
-        System.out.println("OOPS");
+        // UC2: Print Banner using + operator
+        // Line 1
+        System.out.println("  *** " + "   " + "  *** " + "   " + " **** " + "   " + "  **** ");
+        // Line 2
+        System.out.println(" ** ** " + "   " + " ** ** " + "   " + " ** ** " + "   " + " ** ");
+        // Line 3
+        System.out.println(" ** ** " + "   " + " ** ** " + "   " + " ** ** " + "   " + " ** ");
+        // Line 4
+        System.out.println(" ** ** " + "   " + " ** ** " + "   " + " **** " + "   " + "  *** ");
+        // Line 5
+        System.out.println(" ** ** " + "   " + " ** ** " + "   " + " ** " + "   " + "     **");
+        // Line 6
+        System.out.println(" ** ** " + "   " + " ** ** " + "   " + " ** " + "   " + "     **");
+        // Line 7
+        System.out.println("  *** " + "   " + "  *** " + "   " + " ** " + "   " + " **** ");
     }
 }
