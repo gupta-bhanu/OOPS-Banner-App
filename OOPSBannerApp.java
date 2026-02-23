8137529e659966c2e10e3ebe06c63ff57ba2d007
BHANU GUPTA <guptabhanu3636@gmail.com>
2026-02-23 08:15:13 +0530

    UC4: Refactor using Arrays and Loops

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,19 +1,22 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        // UC2: Print Banner using + operator
-        // Line 1
-        System.out.println("  *** " + "   " + "  *** " + "   " + " **** " + "   " + "  **** ");
-        // Line 2
-        System.out.println(" ** ** " + "   " + " ** ** " + "   " + " ** ** " + "   " + " ** ");
-        // Line 3
-        System.out.println(" ** ** " + "   " + " ** ** " + "   " + " ** ** " + "   " + " ** ");
-        // Line 4
-        System.out.println(" ** ** " + "   " + " ** ** " + "   " + " **** " + "   " + "  *** ");
-        // Line 5
-        System.out.println(" ** ** " + "   " + " ** ** " + "   " + " ** " + "   " + "     **");
-        // Line 6
-        System.out.println(" ** ** " + "   " + " ** ** " + "   " + " ** " + "   " + "     **");
-        // Line 7
-        System.out.println("  *** " + "   " + "  *** " + "   " + " ** " + "   " + " **** ");
+        // UC4: Refactoring to use Arrays and Loops for better modularity
+        
+        // Step 1: Store all the banner lines in a String Array
+        // We use String.join inside the array for maximum efficiency
+        String[] bannerLines = {
+            String.join("", "  *** ", "   ", "  *** ", "   ", " **** ", "   ", "  **** "),
+            String.join("", " ** ** ", "   ", " ** ** ", "   ", " ** ** ", "   ", " ** "),
+            String.join("", " ** ** ", "   ", " ** ** ", "   ", " ** ** ", "   ", " ** "),
+            String.join("", " ** ** ", "   ", " ** ** ", "   ", " **** ", "   ", "  *** "),
+            String.join("", " ** ** ", "   ", " ** ** ", "   ", " ** ", "   ", "     ** "),
+            String.join("", " ** ** ", "   ", " ** ** ", "   ", " ** ", "   ", "     ** "),
+            String.join("", "  *** ", "   ", "  *** ", "   ", " ** ", "   ", " **** ")
+        };
+
+        // Step 2: Use an Enhanced For-Loop to iterate and print
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
     }
 }
